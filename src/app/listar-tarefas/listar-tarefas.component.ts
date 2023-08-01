f89e0ff780bd5d48330dd8c8d3722e1e695fb106
Wesley Santos <wvsantos15@gmail.com>
2023-08-01 02:07:44 -0300

    Criação do formulário para listar tarefas, configuração de layout

--- src/app/listar-tarefas/listar-tarefas.component.ts
+++ src/app/listar-tarefas/listar-tarefas.component.ts
@@ -1,5 +1,33 @@
 import { Component } from '@angular/core';
 
+interface Country {
+	tipo: string;
+	descricao: string;
+	acoes: string;
+  data_inicial: string;
+  data_conclusao: string;
+  status_tarefa: string;
+}
+
+const COUNTRIES: Country[] = [
+	{
+		tipo: 'Estudar',
+		descricao: 'Estudar para prova!',
+		acoes: 'Marcar como concluída',
+    data_inicial: '01/08/2023',
+    data_conclusao: '15/08/2023',
+    status_tarefa: 'Em andamento',
+	},
+  {
+		tipo: 'Desenvolver',
+		descricao: 'Desenvolver um site',
+		acoes: 'Marcar como concluída',
+    data_inicial: '02/08/2023',
+    data_conclusao: '02/09/2023',
+    status_tarefa: 'Em andamento',
+	},
+];
+
 @Component({
   selector: 'app-listar-tarefas',
   templateUrl: './listar-tarefas.component.html',
@@ -7,4 +35,8 @@ import { Component } from '@angular/core';
 })
 export class ListarTarefasComponent {
 
+  constructor(){
+
+  }
+  countries = COUNTRIES;
 }

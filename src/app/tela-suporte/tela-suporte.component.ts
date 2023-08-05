da87d2362da104c0360a3f8a51f5d78f1f527de5
Wesley Santos <wvsantos15@gmail.com>
2023-08-05 16:57:41 -0300

    Criação do formulário de suporte com envio do chamado pelo whatsapp

--- src/app/tela-suporte/tela-suporte.component.ts
+++ src/app/tela-suporte/tela-suporte.component.ts
@@ -1,3 +1,4 @@
+import { FormsModule } from '@angular/forms';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./tela-suporte.component.scss']
 })
 export class TelaSuporteComponent {
+  nome: string = '';
+  email: string = '';
+  mensagem: string = '';
 
+  constructor(form: FormsModule){}
+
+  enviarMensagem() {
+    const mensagem = `Olá, me chamo ${this.nome}, meu email é o ${this.email}. Gostaria de resolver o meu problema descrito a seguir: *${this.mensagem}*`;
+    const urlWhatsapp = `https://api.whatsapp.com/send?phone=5579998141623&text=${encodeURIComponent(mensagem)}`;
+
+    // Abre o link do WhatsApp no navegador
+    window.open(urlWhatsapp);
+  }
 }
